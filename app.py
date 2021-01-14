180f8446a5efacf19726e247c9d8c54055ef144e
Mounah12 <77074782+Mounah12@users.noreply.github.com>
2021-01-14 11:07:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -35,5 +35,4 @@ def predict_api():
     return jsonify(output)
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0',port=port)
+    app.run(debug=True)
